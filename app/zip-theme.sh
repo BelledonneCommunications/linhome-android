78341c36544545c0798f27c0de2ed82d06a0762d
Christophe Deschamps <christophe.deschamps.work@gmail.com>
2020-06-24 16:48:16 +0200

    Added theming from lindoor-shared-themes
    Former-commit-id: 86d78dbe35c14dab612754ab4b8e459c2cf24cd3

---
+++ app/zip-theme.sh
@@ -0,0 +1,4 @@
+cd ../lindoor-shared-themes
+zip -r lindoor.zip action_types.xml method_types.xml device_types.xml texts.xml theme.xml images fonts  
+cp lindoor.zip ../app/src/main/assets/lindoor.zip
+

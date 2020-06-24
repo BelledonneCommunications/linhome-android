0185e75b7b783d08859668351e9daf4e3bee306f
Christophe Deschamps <christophe.deschamps.work@gmail.com>
2020-06-24 19:08:58 +0200

    Clickable GPL Link
    Former-commit-id: 91a23ec07d4c7b5bc2bf3c027d0ef53b48f62a82

--- app/src/main/java/org/lindoor/ui/about/AboutFragment.kt
+++ app/src/main/java/org/lindoor/ui/about/AboutFragment.kt
@@ -1,11 +1,15 @@
 package org.lindoor.ui.about
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_about.view.*
 import org.lindoor.LindoorApplication
+import org.lindoor.customisation.Texts
 import org.lindoor.databinding.FragmentAboutBinding
 
 class AboutFragment : Fragment() {
@@ -17,6 +21,16 @@ class AboutFragment : Fragment() {
     ): View? {
         val binding = FragmentAboutBinding.inflate(inflater, container, false)
         binding.coreContext = LindoorApplication.coreContext
+
+
+        binding.root.licence.setOnClickListener {
+            val browserIntent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse(Texts.get("license_link"))
+            )
+            startActivity(browserIntent)
+        }
+
         return binding.root
     }
 }

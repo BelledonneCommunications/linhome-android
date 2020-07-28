0bd7f2cf4fd0285f451ae17bb067583b41bc988d
Christophe Deschamps <christophe.deschamps.work@gmail.com>
2020-07-28 10:17:12 +0200

    Moved the zip task to gradle
    Former-commit-id: 78e6b7e9e2155524b138da9c81a364036b8c42a1

--- app/zip-theme.sh
+++
@@ -1,4 +0,0 @@
-cd ../lindoor-shared-themes
-zip -r lindoor.zip action_types.xml method_types.xml device_types.xml texts.xml theme.xml images fonts  
-cp lindoor.zip ../app/src/main/assets/lindoor.zip
-
